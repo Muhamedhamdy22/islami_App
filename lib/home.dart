a42e02616fc6e16eaa4c4a8c00a561a102b2877d
7amdy22 <Muhamedhamdy22@example.com>
2025-12-15 03:26:48 -0800

    splash Screen Added

--- lib/home.dart
+++ lib/home.dart
@@ -6,6 +6,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text("data")),
+    );
   }
 }
